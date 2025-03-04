8cfbe70a4623d324479b52a296e39233ac6e14b7
JJ <fidojj@hotmail.com>
2025-03-04 15:15:27 +1100

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div class="w-screen h-screen bg-ajc-beige-500 text-ajc-text">
+            <div className="w-screen h-screen bg-ajc-beige-500 text-ajc-text">
+                <div className="flex flex-col items-center h-full pt-10">
+                    <img src="/assets/ajclogo.png" alt="logo" class="h-48" />
 
+                    <div className="flex items-center gap-x-10">
+                        <div className="rounded-lg w-10 h-10 bg-black"></div>
+                        <div className="rounded-lg w-10 h-10 bg-black"></div>
+                        <div className="rounded-lg w-10 h-10 bg-black"></div>
+                        <div className="rounded-lg w-10 h-10 bg-black"></div>
+                    </div>
+                </div>
             </div>
         </>
     )
