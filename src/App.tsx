e065baa1d9074b9e4b671057f88c5202eede1844
JJ <fidojj@hotmail.com>
2025-03-04 14:42:14 +1100

    Configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div class="w-screen h-screen bg-ajc-beige-500">
+    return (
+        <>
+            <div class="w-screen h-screen bg-ajc-beige-500 text-ajc-text">
 
-      </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
 
 export default App
