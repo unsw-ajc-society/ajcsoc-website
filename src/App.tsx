866069efb0d4ec1dff6c401b0ae4c0b1f2f3505e
JJ <fidojj@hotmail.com>
2025-04-11 18:10:56 +1000

    social icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import './App.css';
 
+import { FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
+
 function App() {
     return (
         <>
             <div className="w-screen h-screen bg-ajc-beige-500 text-ajc-text">
-                <div className="flex flex-col items-center h-full pt-10">
-                    <img src="/assets/ajclogo.png" alt="logo" class="h-48" />
+                <div className="flex flex-col items-center h-full pt-10 gap-y-5">
+                    <img src="/assets/ajclogo.png" alt="logo" className="h-36" />
 
                     <div className="flex items-center gap-x-10">
-                        <div className="rounded-lg w-10 h-10 bg-black"></div>
-                        <div className="rounded-lg w-10 h-10 bg-black"></div>
-                        <div className="rounded-lg w-10 h-10 bg-black"></div>
-                        <div className="rounded-lg w-10 h-10 bg-black"></div>
+                        <span className="text-ajc-text text-xl">ホーム</span>
+                        <span className="text-ajc-text text-xl">サークルについて</span>
+                        <span className="text-ajc-text text-xl">イベント</span>
+                    </div>
+
+                    <div className="flex items-center gap-x-10">
+                        <FaInstagram className="w-10 h-10" />
+                        <FaFacebook className="w-10 h-10" />
+                        <FaLinkedin className="w-10 h-10" />
                     </div>
                 </div>
             </div>
