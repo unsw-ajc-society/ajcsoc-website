ff900585387df54d4e54bbdd2554b0d8a95ae918
Niina Kato <z5675718@ad.unsw.edu.au>
2025-05-23 15:16:33 +1000

    fixed the logo size a little smaller

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="w-full h-full bg-ajc-beige-500 text-ajc-text">
             <div className="flex flex-col items-center h-full pt-10 gap-y-5">
-                <img src="/assets/ajclogo.png" alt="logo" style={{ width: '80vw', height: 'auto' }} />
+                <img src="/assets/ajclogo.png" alt="logo" style={{ width: '75vw', height: 'auto' }} />
 
                 {/* <div className="flex items-center gap-x-10">
                     <span className="text-ajc-text text-xl">ホーム</span>
