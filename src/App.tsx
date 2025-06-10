485959837fc7c563f3d23c150fcf783c02021810
Sarah Ho <z5481589@ad.unsw.edu.au>
2025-06-10 18:12:25 +1000

    deleted o-week stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import AboutUs from './AboutUs';
 import './App.css';
 import Icons from './Icon';
 import Events from './Events'
-import T2Welcome from './T2Welcome'
+// import T2Welcome from './T2Welcome' <- save for T3
 
 function App() {
     return (
         <div className="w-full h-full bg-ajc-beige-500 text-ajc-text">
             <div className="flex flex-col items-center h-full">
-                <img src="/assets/ajclogo.png" alt="logo" className="w-[62vw] h-auto" />
+                <img src="/assets/ajclogo.png" alt="AJCsoc UNSW logo" className="w-[62vw] h-auto" />
                 <Icons/>
                 <AboutUs/>
-                <T2Welcome/>
+                {/* <T2Welcome/> */}
                 <Events/>
                 <h2 className= "mt-15 ajc-text text-bold text-4xl font-bold text-ajc-text mb-10 text-center">
                     Connect with us and stay tuned~
