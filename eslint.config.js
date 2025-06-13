aa4b96048b0a6c1af8019f1debc26a6537d54aed
Sarah Ho <z5481589@ad.unsw.edu.au>
2025-06-13 23:41:37 +1000

    added metadata and opengraph

--- eslint.config.js
+++ eslint.config.js
@@ -4,31 +4,31 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 
 export default [
-	{ ignores: ["dist"] },
-	{
-		files: ["**/*.{js,jsx}"],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-			parserOptions: {
-				ecmaVersion: "latest",
-				ecmaFeatures: { jsx: true },
-				sourceType: "module",
-			},
-		},
-		plugins: {
-			"react-hooks": reactHooks,
-			"react-refresh": reactRefresh,
-		},
-		rules: {
-			...js.configs.recommended.rules,
-			...reactHooks.configs.recommended.rules,
-			"no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-			"react-refresh/only-export-components": [
-				"warn",
-				{ allowConstantExport: true },
-			],
-			indent: ["error", 4],
-		},
-	},
+    { ignores: ["dist"] },
+    {
+        files: ["**/*.{js,jsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                ecmaFeatures: { jsx: true },
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+            indent: ["error", 4],
+        },
+    },
 ];
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 //import { useState } from "react"; ← not used
+import { Helmet } from "react-helmet-async";
 import AboutUs from "./AboutUs";
 import PastEvents from "./PastEvents";
 import Icons from "./Icon";
@@ -10,6 +11,26 @@ import UpcomingSocialEvents from "./UpcomingSocialEvents";
 
 function App() {
 	return (
+		<>
+		<Helmet>
+			<title>
+				AJC society UNSW
+			</title>
+			<meta 
+				name="description"
+				content="Australia-Japan Career Development Society is focused on helping students
+				build amazing careers, whether that's in Japan, with Japanese-affiliated (Nikkei)
+				companies, or right here in Australia for our community."
+			/>
+			<meta 
+				property="og:description"
+				content="Build a global career! Join AJC UNSW and connect with opportunities in Japan and Australia."
+			/>
+			<meta property="og:title" content="AJC society UNSW" />
+  			<meta property="og:image" content="./assets/ajclogo.png" />
+  			<meta property="og:url" content="https://ajcsoc.org" />
+  			<meta property="og:type" content="website" />
+		</Helmet>
 		<div className="w-full h-full bg-ajc-beige-500 text-ajc-text">
 			<div className="flex flex-col items-center h-full">
 				<img src="/assets/ajclogo.png" alt="logo" className="w-[62vw] h-auto" />
@@ -27,6 +48,7 @@ function App() {
 				<div className="mt-15"></div>
 			</div>
 		</div>
+		</>
 	);
 }
 
