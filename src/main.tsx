f9d516a0a299abf4be5e7dabc2736057546a9b24
Sarah Ho <z5481589@ad.unsw.edu.au>
2025-06-16 15:06:26 +1000

    testing server side rendering with next.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import App from "./App.js";
+import App from "../pages/index.js";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
