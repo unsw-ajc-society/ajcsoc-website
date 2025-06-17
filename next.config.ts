9ebc403a3f131efc454721e84440a58becf52c08
Risu <79110363+risu729@users.noreply.github.com>
2025-06-17 11:34:26 +1000

    Revert "disable inlineCss as not worked"
    This reverts commit e5a690c7e36d7e2d46631aaa8bc8b439c09f48ee.

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
 	experimental: {
+		// Enable since we only have single page
+		// ref: https://nextjs.org/docs/app/api-reference/config/next-config-js/inlineCss
+		inlineCss: true,
 		typedRoutes: true,
 	},
 	typescript: {
