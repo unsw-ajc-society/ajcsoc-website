0f478082a2c0c9af83e3a12f070f3b2e22c658f6
Risu <79110363+risu729@users.noreply.github.com>
2025-06-17 10:24:15 +1000

    enable experimental features of next

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	experimental: {
+		// Enable since we only have single page
+		// ref: https://nextjs.org/docs/app/api-reference/config/next-config-js/inlineCss
+		inlineCss: true,
+		typedRoutes: true,
+	},
 	typescript: {
 		tsconfigPath: "./tsconfig.app.json",
 	},
