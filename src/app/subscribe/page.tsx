cffeb99d795cac2b16c94db8417fed5a2cc719b3
Risu <79110363+risu729@users.noreply.github.com>
2025-06-24 04:38:14 +1000

    fix: remove unnecessary route

--- src/app/subscribe/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Subscribe() {
-	// TODO: Implement direct subscription page
-	// For now, do not allow direct access to the subscription page
-	redirect("/");
-}
