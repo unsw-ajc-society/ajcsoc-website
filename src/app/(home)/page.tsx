131c20ee2e71bb30892b53272dd3c036c6e5a932
Sarah Ho <z5481589@ad.unsw.edu.au>
2025-06-25 02:12:21 +1000

    trying to fix build..

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -17,14 +17,6 @@ export default function Home() {
 				src={ajcLogo}
 			/>
 			<SocialIcons />
-			{/* prefetch of dynamic routes is disabled by default */}
-			{/* <Link
-				className="mx-auto inline-block min-w-[250px] whitespace-nowrap rounded bg-primary px-2 py-2 text-center text-white uppercase shadow hover:scale-110 hover:shadow-lg"
-				href="/subscribe"
-				prefetch={true}
-			>
-				Subscribe to our Newsletter
-			</Link> */}
 			<AboutUs />
 			<UpcomingCareerEvents />
 			<UpcomingSocialEvents />
