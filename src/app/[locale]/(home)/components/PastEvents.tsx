435f7c28383a3902b1437e430012baf6ca2224d0
Tatsuya200 <268640905+Tatsuya200@users.noreply.github.com>
2026-06-27 20:00:03 +1000

    updated pastEvents in its tsx file and added photos in images folder

--- src/app/[locale]/(home)/components/PastEvents.tsx
+++ src/app/[locale]/(home)/components/PastEvents.tsx
@@ -1,27 +1,23 @@
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import { Link } from "../../../../i18n/navigation";
-import HAndHEvent from "../images/events/H&H_Event.png";
-import KPMGEvent from "../images/events/KPMG_Event.jpg";
-import MyNaviEvent from "../images/events/MyNavi_Event.jpg";
+import PubCrawlEvent from "../images/events/pub-crawl.jpg";
+import VividEvent from "../images/events/vivid-event.jpg";
+import T2_Oday from "../images/events/26t2-oday.jpg";
 
 const pastEvents = [
 	{
-		href: "https://docs.google.com/forms/d/e/1FAIpQLSfC_Zc8xWA-dJJYbjJJUL5sr-LtRgrAgrmsCc6A2UAVw-obng/viewform",
-		id: "hnh",
-		image: HAndHEvent,
+		id: "pub-crawl",
+		image: PubCrawlEvent,
 		translationKey: "networking",
 	},
 	{
-		href: "https://regu-20105603.hs-sites.com/kpmg-aus25",
-		id: "kpmg",
-		image: KPMGEvent,
+		id: "vivid-event",
+		image: VividEvent,
 		translationKey: "networking",
 	},
 	{
-		href: "https://global.mynavi.jp/conts/event/aus_mcs2025jul/?utm_source=studentpartnar&utm_medium=textl&utm_campaign=2507",
-		id: "mynavi",
-		image: MyNaviEvent,
+		id: "26t2-oday",
+		image: T2_Oday,
 		translationKey: "networking",
 	},
 ] as const;
@@ -34,22 +30,19 @@ export default function PastEvents() {
 			<div className="mx-auto flex w-full flex-col items-center">
 				<h2 className="mb-10 text-center text-2xl font-bold md:text-4xl">{t("title")}</h2>
 				<div className="flex w-full flex-wrap justify-center gap-[4vw]">
-					{pastEvents.map(({ id, translationKey, href, image }) => (
-						<Link
-							className="card group w-[26%] min-w-[140px] overflow-hidden bg-base-100 shadow-md transition duration-300 hover:-translate-y-1 hover:shadow-xl"
-							href={href}
+					{pastEvents.map(({ id, translationKey, image }) => (
+						<div
+							className="card w-[26%] min-w-[140px] overflow-hidden bg-base-100 shadow-md"
 							key={id}
-							rel="noopener noreferrer"
-							target="_blank"
 						>
 							<figure>
 								<Image
 									alt={t(`events.${translationKey}`)}
-									className="h-full w-full object-contain transition-transform duration-300 group-hover:scale-105"
+									className="h-full w-full object-contain"
 									src={image}
 								/>
 							</figure>
-						</Link>
+						</div>
 					))}
 				</div>
 			</div>
